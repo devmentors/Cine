063e76e0a1d7ae60d4ee7ed6ffa3b860b2b5678d
GooRiOn <foreverframenet@gmail.com>
2019-12-08 13:14:35 +0100

    Added modules

---
+++ src/Modules/Cinema.Modules.Movies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cinema.Modules.Movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
