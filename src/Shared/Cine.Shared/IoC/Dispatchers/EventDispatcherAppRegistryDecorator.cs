b51e1a43d5416d08b6d0deaf799d1d2a67823372
Dariusz Pawlukiewicz <foreverframenet@gmail.com>
2020-01-21 22:37:33 +0100

    Added event dispatcher

---
+++ src/Shared/Cine.Shared/IoC/Dispatchers/EventDispatcherAppRegistryDecorator.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Cine.Shared.IoC.Registries;
+using Convey.CQRS.Events;
+using Newtonsoft.Json;
+
+namespace Cine.Shared.IoC.Dispatchers
+{
+    internal sealed class EventDispatcherAppRegistryDecorator : IEventDispatcher
+    {
+        private readonly IEventDispatcher _dispatcher;
+        private readonly IAppTypesRegistry _registry;
+
+        public EventDispatcherAppRegistryDecorator(IEventDispatcher dispatcher, IAppTypesRegistry registry)
+        {
+            _dispatcher = dispatcher;
+            _registry = registry;
+        }
+
+        public async Task PublishAsync<T>(T @event) where T : class, IEvent
+        {
+            var eventTypes = _registry.GetLocalTypes(@event.GetType());
+
+            foreach (var type in eventTypes)
+            {
+                var json = JsonConvert.SerializeObject(@event);
+                var message = JsonConvert.DeserializeObject(json, type);
+
+                await (Task)_dispatcher.GetType()
+                    .GetMethod(nameof(_dispatcher.PublishAsync))
+                    .MakeGenericMethod(type)
+                    .Invoke(_dispatcher, new object[] { message});
+            }
+        }
+    }
+}
