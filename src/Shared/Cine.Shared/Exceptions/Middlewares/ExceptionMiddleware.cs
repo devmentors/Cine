9e8ffad744088b9a832460cc861ce2517ccf4442
GooRiOn <foreverframenet@gmail.com>
2020-04-26 14:52:40 +0200

    Added logging to exception middleware

--- src/Shared/Cine.Shared/Exceptions/Middlewares/ExceptionMiddleware.cs
+++ src/Shared/Cine.Shared/Exceptions/Middlewares/ExceptionMiddleware.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Cine.Shared.Exceptions.Mappers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
@@ -12,11 +13,14 @@ namespace Cine.Shared.Exceptions.Middlewares
     {
         private readonly RequestDelegate _next;
         private readonly IExceptionCompositionRoot _exceptionCompositionRoot;
+        private readonly ILogger<ExceptionMiddleware> _logger;
 
-        public ExceptionMiddleware(RequestDelegate next, IExceptionCompositionRoot exceptionCompositionRoot)
+        public ExceptionMiddleware(RequestDelegate next, IExceptionCompositionRoot exceptionCompositionRoot,
+            ILogger<ExceptionMiddleware> logger)
         {
             _next = next;
             _exceptionCompositionRoot = exceptionCompositionRoot;
+            _logger = logger;
         }
 
         public async Task Invoke(HttpContext httpContext)
@@ -31,13 +35,19 @@ namespace Cine.Shared.Exceptions.Middlewares
 
                 if (responseData is { })
                 {
-                    var json = JsonConvert.SerializeObject(new { Code = responseData.Code, Message = responseData.Message });
+                    _logger.LogError($"An error has occured while processing the request " +
+                                     $"{httpContext.Request.Path}. The error code: [{responseData.Code}] " +
+                                     $"with message {responseData.Message}");
+
+                    var json = JsonConvert.SerializeObject(new {responseData.Code, responseData.Message });
                     httpContext.Response.StatusCode = (int) responseData.HttpStatus;
                     httpContext.Response.Headers.Add("content-type", "application/json");
                     await httpContext.Response.WriteAsync(json);
                 }
                 else
                 {
+                    _logger.LogError($"An unhandled exception will be throw while processing the request " +
+                                     $"{httpContext.Request.Path}");
                     throw;
                 }
             }
