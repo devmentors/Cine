a2963300a6955d67a34a3a2d925e0f2e285a8e54
GooRiOn <foreverframenet@gmail.com>
2020-04-26 15:02:06 +0200

    Added logging to event processort

--- src/Shared/Cine.Shared/Events/EventProcessor.cs
+++ src/Shared/Cine.Shared/Events/EventProcessor.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Cine.Shared.BuildingBlocks;
 using Cine.Shared.MessageBrokers;
 using Convey.CQRS.Events;
+using Microsoft.Extensions.Logging;
 
 namespace Cine.Shared.Events
 {
@@ -10,11 +11,14 @@ namespace Cine.Shared.Events
     {
         private readonly IMessageBroker _broker;
         private readonly IEventMapperCompositionRoot _eventMapperCompositionRoot;
+        private readonly ILogger<IEventProcessor> _logger;
 
-        public EventProcessor(IMessageBroker broker, IEventMapperCompositionRoot eventMapperCompositionRoot)
+        public EventProcessor(IMessageBroker broker, IEventMapperCompositionRoot eventMapperCompositionRoot,
+            ILogger<IEventProcessor> logger)
         {
             _broker = broker;
             _eventMapperCompositionRoot = eventMapperCompositionRoot;
+            _logger = logger;
         }
 
         public async Task ProcessAsync(IEnumerable<IDomainEvent> events)
@@ -28,6 +32,8 @@ namespace Cine.Shared.Events
 
             foreach (var @event in events)
             {
+                _logger.LogTrace($"Processing domain event of type {{{@event.GetType().Name}}}");
+
                 var integrationEvent = _eventMapperCompositionRoot.Map(@event);
 
                 if (integrationEvent is null)
@@ -36,6 +42,8 @@ namespace Cine.Shared.Events
                 }
 
                 integrationEvents.Add(integrationEvent);
+                _logger.LogTrace($"Mapped domain event of type {{{@event.GetType().Name}}} to integration " +
+                                 $"event of type {integrationEvent.GetType().Name}");
             }
 
             await _broker.PublishAsync(integrationEvents);
